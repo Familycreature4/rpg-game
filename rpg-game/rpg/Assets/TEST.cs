35dbb17990889ab407bc7d40d0d78a1165cded08
infinite-eito <111705756+infinite-eito@users.noreply.github.com>
2022-08-21 13:28:03 -0700

    inf cube test 2

--- rpg-game/rpg/Assets/TEST.cs
+++ rpg-game/rpg/Assets/TEST.cs
@@ -7,7 +7,7 @@ public class TEST : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("INFINITE IS GAY!!!!!!!!!");
+        Debug.Log("Actually, Familycreature is gay. QED");
     }
 
     // Update is called once per frame
