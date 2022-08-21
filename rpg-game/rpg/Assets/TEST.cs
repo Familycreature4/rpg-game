4b551cf18cc250fbbb6481bf1c756b1f97aa5814
infinite-eito <111705756+infinite-eito@users.noreply.github.com>
2022-08-21 13:31:47 -0700

    Revert "inf cube test 2"
    This reverts commit 35dbb17990889ab407bc7d40d0d78a1165cded08.

--- rpg-game/rpg/Assets/TEST.cs
+++ rpg-game/rpg/Assets/TEST.cs
@@ -7,7 +7,7 @@ public class TEST : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Actually, Familycreature is gay. QED");
+        Debug.Log("INFINITE IS GAY!!!!!!!!!");
     }
 
     // Update is called once per frame
